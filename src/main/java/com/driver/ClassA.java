a8f4809c6d0e4d15ad511da02fcdf0bf6ab845c1
nithish071 <nitheeshkumar124@gmail.com>
2024-03-20 15:37:33 +0530

    Update

---
+++ src/main/java/com/driver/ClassA.java
@@ -0,0 +1,9 @@
+package com.driver;
+
+public class ClassA {
+
+
+    public String meth(){
+        return "Invoking method from class A";
+    }
+}
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        ClassB classB = new ClassB();
+        System.out.println(classB.meth());
+    }
   
 }
